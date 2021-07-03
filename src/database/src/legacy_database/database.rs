b01486e008f1c0f3f8f2ae1ad7131f789b027b22
btwotwo <10519967+btwotwo@users.noreply.github.com>
2021-07-03 12:21:11 +0000

    Add reference creation to the legacy database

--- src/database/src/legacy_database/database.rs
+++ src/database/src/legacy_database/database.rs
@@ -1,4 +1,6 @@
-use super::chunk::{Chunk, ChunkError};
+use std::io::{self, BufReader};
+
+use super::{chunk::{Chunk, ChunkError}, index::{Reference, serialized::IndexCollection}};
 use crate::{post::Post, post_database::Database};
 use thiserror::Error;
 
@@ -9,18 +11,37 @@ pub enum LegacyDatabaseError {
         #[from]
         source: ChunkError,
     },
+
+    #[error("IO error")]
+    IoError {
+        #[from]
+        source: io::Error
+    },
+
+    #[error("Serde error")]
+    SerdeError {
+        #[from]
+        source: serde_json::Error
+    }
 }
 
+const INDEX_FILENAME: &str = "index-3.json";
+const DIFF_FILENAME: &str = "diff-3.list";
 pub type LegacyDatabaseResult<T> = Result<T, LegacyDatabaseError>;
 
 struct LegacyDatabase {
     current_chunk: Chunk,
+    reference: Reference
 }
 
 impl LegacyDatabase {
-    pub fn new() -> LegacyDatabaseResult<Self> {
+    pub fn new(index_file: std::fs::File) -> LegacyDatabaseResult<Self> {
+        let index: IndexCollection = serde_json::from_reader(BufReader::new(index_file))?;
+        let reference= Reference::new(index);
+
         Ok(LegacyDatabase {
             current_chunk: Chunk::try_new(None)?,
+            reference
         })
     }
 }
