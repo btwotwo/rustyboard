e1827c4bf30c9b16a7ee019314e06823ef222e05
btwotwo <10519967+btwotwo@users.noreply.github.com>
2021-07-05 15:58:25 +0000

    Add helper function to Post

--- src/database/src/post.rs
+++ src/database/src/post.rs
@@ -1,5 +1,11 @@
 pub struct Post {
-    hash: String,
-    message: String,
-    reply_to: String,
+    pub hash: String,
+    pub message: String,
+    pub reply_to: String,
+}
+
+impl Post {
+    pub fn get_bytes(&self) -> Vec<u8> {
+        base64::decode(&self.message).unwrap() //todo replace with error handling;
+    }
 }
