121e66331cc19ae184292ac9843e20247d6443c6
btwotwo <10519967+btwotwo@users.noreply.github.com>
2021-07-08 20:07:07 +0000

    Add `ChunkProcessor` which connects `Post`s and `Chunk`s
    This structure\trait allows us to abstract the process of writing post data into chunks, while trait allows us to switch to other implementations (for example keep chunks in memory)

---
+++ src/database/src/legacy_database/chunk_processor.rs
@@ -0,0 +1,64 @@
+use std::{convert::TryFrom, error::Error};
+
+use crate::post::Post;
+
+use super::{chunk::{Chunk, ChunkError::{self, ChunkTooLarge}, ChunkIndex}, index::db_post_ref::{ChunkSettings, DbPostRef}};
+use thiserror::Error;
+
+pub trait ChunkCollectionProcessor {
+    type Error: Error;
+
+    fn insert(&mut self, post: &Post) -> Result<ChunkSettings, Self::Error>;
+    fn insert_into_existing(&mut self, chunk: &ChunkSettings, post: &Post) -> Result<(), Self::Error>;
+}
+
+pub struct OnDiskChunkCollectionProcessor {
+    last_chunk: Chunk,
+}
+
+#[derive(Debug, Error)]
+pub enum OnDiskChunkCollectionProcessorError {
+    #[error("Chunk error")]
+    ChunkError {
+        #[from]
+        source: ChunkError
+    }
+}
+
+impl OnDiskChunkCollectionProcessor {
+    fn extend_current_chunk(&mut self) -> Result<(), OnDiskChunkCollectionProcessorError> {
+        let new_chunk = self.last_chunk.create_extended()?;
+        self.last_chunk = new_chunk;
+        Ok(())
+    }
+}
+
+impl ChunkCollectionProcessor for OnDiskChunkCollectionProcessor {
+    type Error = OnDiskChunkCollectionProcessorError;
+
+    fn insert(&mut self, post: &Post) -> Result<ChunkSettings, Self::Error> {
+        let post_bytes = post.get_bytes();
+        let result = self.last_chunk.try_append_data(&post_bytes);
+        match result {
+            Ok(offset) => Ok(ChunkSettings {
+                chunk_index: self.last_chunk.index,
+                offset
+            }),
+            Err(err) => match err {
+                ChunkTooLarge => {
+                    self.extend_current_chunk()?;
+                    self.insert(post)
+                },
+                _ => Err(err.into())
+            }
+        }
+    }
+
+    fn insert_into_existing(&mut self, settings: &ChunkSettings, post: &Post) -> Result<(), Self::Error> {
+        let post_bytes = post.get_bytes();
+        let mut chunk = Chunk::open(settings.chunk_index)?;
+        chunk.try_write_data(&post_bytes, settings.offset)?;
+        Ok(())
+    }
+
+}
--- src/database/src/legacy_database/mod.rs
+++ src/database/src/legacy_database/mod.rs
@@ -1,3 +1,4 @@
 pub mod chunk;
 pub mod database;
 pub mod index;
+mod chunk_processor;
