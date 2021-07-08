6bbe4f56369ab85d36c866c661be38e9146f5420
btwotwo <10519967+btwotwo@users.noreply.github.com>
2021-07-08 20:04:30 +0000

    Add associated error type for the Database trait

--- src/database/src/post_database.rs
+++ src/database/src/post_database.rs
@@ -1,7 +1,9 @@
 use std::error::Error;
 use crate::post::Post;
 
-pub trait Database<TError: Error> {
-    fn put_post(&mut self, post: Post, allow_reput: bool) -> Result<(), TError>;
+pub trait Database {
+    type Error: Error;
+
+    fn put_post(&mut self, post: Post, allow_reput: bool) -> Result<(), Self::Error>;
     fn get_post(&self, hash: String) -> Option<Post>;
 }
