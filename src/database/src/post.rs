772eddf205a219b0f80bfecd3489ed16a9eeea53
btwotwo <10519967+btwotwo@users.noreply.github.com>
2021-07-18 11:09:12 +0000

    Add constructor to post, add Clone impl

--- src/database/src/post.rs
+++ src/database/src/post.rs
@@ -1,3 +1,4 @@
+#[derive(Clone)]
 pub struct Post {
     pub hash: String,
     pub reply_to: String,
@@ -6,7 +7,15 @@ pub struct Post {
 }
 
 impl Post {
-    pub fn get_bytes(&self) -> Vec<u8> {
+    pub fn new(hash: String, reply_to: String, raw_message: String) -> Self {
+        Self {
+            hash,
+            reply_to,
+            message: base64::encode(raw_message),
+        }
+    }
+
+    pub fn get_message_bytes(&self) -> Vec<u8> {
         base64::decode(&self.message).unwrap() //todo replace with error handling;
     }
 }
