7949cd0d1a6ca8f4305ef93fc8222e9149a7cf6f
btwotwo <10519967+btwotwo@users.noreply.github.com>
2021-07-20 19:26:24 +0000

    Temporary diff file operation removal

--- src/database/src/legacy_database/index/diff.rs
+++ src/database/src/legacy_database/index/diff.rs
@@ -1,7 +1,9 @@
-use std::{fs::File, io::Write};
+use std::{fs::{File, OpenOptions}, io::Write};
 
 use super::{db_post_ref::DbPostRef, serialized::{DbPostRefSerialized, PostHashes}};
 use thiserror::Error;
+
+const DIFF_FILENAME: &str = "diff-3.list";
 
 #[derive(Debug, Error)]
 pub enum DiffFileError {
@@ -14,15 +16,23 @@ pub enum DiffFileError {
 pub type DiffResult<T> = Result<T, DiffFileError>;
 pub trait Diff {
     fn append(&mut self, hashes: PostHashes, db_ref: DbPostRef) -> DiffResult<(PostHashes, DbPostRef)>;   
+    fn new() -> Self where Self: Sized;
 }
 
-struct DiffFile(File);
+//todo: Open file for every write, or force user to apply the existing diff to the database before opening a new one?
+//todo: test error: thread 'legacy_database::index::tests::new::when_contains_deleted_post_should_add_to_deleted' has overflowed its stack
+
+pub struct DiffFile;
 impl Diff for DiffFile {
     fn append(&mut self, hashes: PostHashes, db_ref: DbPostRef) -> DiffResult<(PostHashes, DbPostRef)> {
         let serialized_obj = DbPostRefSerialized::new(hashes, db_ref);
         let serialized_string = serialized_obj.serialize()?;
-        self.0.write_all(format!("{}\n", serialized_string).as_bytes())?;
+        //self.0.write_all(format!("{}\n", serialized_string).as_bytes())?;
 
         Ok(serialized_obj.split())
+    }
+
+    fn new() -> Self where Self: Sized {
+        DiffFile{}
     }
 }
