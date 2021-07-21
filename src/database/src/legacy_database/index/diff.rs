0c221b4ffaa660757fc4be8cf12ca9b755df5c8f
btwotwo <10519967+btwotwo@users.noreply.github.com>
2021-07-21 21:19:52 +0000

    Update `Diff` trait, make it sized and make `apply_diff` accept `DbRefCollection`

--- src/database/src/legacy_database/index/diff.rs
+++ src/database/src/legacy_database/index/diff.rs
@@ -1,6 +1,14 @@
-use std::{fs::{File, OpenOptions}, io::Write};
+use std::{
+    fs::{File, OpenOptions},
+    io::{BufRead, BufReader, Write},
+    iter,
+};
 
-use super::{db_post_ref::DbPostRef, serialized::{DbPostRefSerialized, PostHashes}};
+use super::{
+    db_post_ref::DbPostRef,
+    serialized::{DbPostRefSerialized, PostHashes},
+    DbRefCollection,
+};
 use thiserror::Error;
 
 const DIFF_FILENAME: &str = "diff-3.list";
@@ -10,21 +18,41 @@ pub enum DiffFileError {
     #[error("Error serializing reference")]
     SerializationError(#[from] serde_json::Error),
     #[error("Error saving reference")]
-    SavingError(#[from] std::io::Error)
+    SavingError(#[from] std::io::Error),
 }
 
 pub type DiffResult<T> = Result<T, DiffFileError>;
-pub trait Diff {
-    fn append(&mut self, hashes: PostHashes, db_ref: DbPostRef) -> DiffResult<(PostHashes, DbPostRef)>;   
-    fn new() -> Self where Self: Sized;
+pub trait Diff: Sized {
+    fn append(
+        &mut self,
+        hashes: PostHashes,
+        db_ref: DbPostRef,
+    ) -> DiffResult<(PostHashes, DbPostRef)>;
+
+    fn apply_diff(&self, coll: &mut DbRefCollection) -> DiffResult<()>;
 }
 
 //todo: Open file for every write, or force user to apply the existing diff to the database before opening a new one?
 //todo: test error: thread 'legacy_database::index::tests::new::when_contains_deleted_post_should_add_to_deleted' has overflowed its stack
 
 pub struct DiffFile;
+
+impl DiffFile {
+    fn deserialize(file: &File) -> Vec<DbPostRefSerialized> {
+        let buf = BufReader::new(file);
+        buf.lines()
+            .map(|l| l.expect("Invalid diff file!"))
+            .map(|line| DbPostRefSerialized::deserialize(&line).expect("Invalid diff file!"))
+            .collect()
+    }
+}
+
 impl Diff for DiffFile {
-    fn append(&mut self, hashes: PostHashes, db_ref: DbPostRef) -> DiffResult<(PostHashes, DbPostRef)> {
+    fn append(
+        &mut self,
+        hashes: PostHashes,
+        db_ref: DbPostRef,
+    ) -> DiffResult<(PostHashes, DbPostRef)> {
         let serialized_obj = DbPostRefSerialized::new(hashes, db_ref);
         let serialized_string = serialized_obj.serialize()?;
         //self.0.write_all(format!("{}\n", serialized_string).as_bytes())?;
@@ -32,7 +60,18 @@ impl Diff for DiffFile {
         Ok(serialized_obj.split())
     }
 
-    fn new() -> Self where Self: Sized {
-        DiffFile{}
+    fn apply_diff(&self, coll: &mut DbRefCollection) -> DiffResult<()> {
+        let diff_file = File::open(DIFF_FILENAME);
+        let refs = match diff_file {
+            Ok(file) => Self::deserialize(&file),
+            Err(io) => match io.kind() {
+                std::io::ErrorKind::NotFound => iter::empty().collect(),
+                _ => return Err(io.into()),
+            },
+        };
+
+        //todo Apply parsed refs to collection
+
+        Ok(())
     }
 }
