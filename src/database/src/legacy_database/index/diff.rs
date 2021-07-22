4dddc61c1821ae3dfca7a9910f988b08a2ebf3a3
btwotwo <10519967+btwotwo@users.noreply.github.com>
2021-07-22 20:05:13 +0000

    Open file for every operation in diff

--- src/database/src/legacy_database/index/diff.rs
+++ src/database/src/legacy_database/index/diff.rs
@@ -1,8 +1,4 @@
-use std::{
-    fs::{File, OpenOptions},
-    io::{BufRead, BufReader, Write},
-    iter,
-};
+use std::{fs::{self, File, OpenOptions}, io::{self, BufRead, BufReader, Write}, iter::{self, FromIterator}, mem, path::{self, Path}};
 
 use super::{
     db_post_ref::DbPostRef,
@@ -25,53 +21,61 @@ pub type DiffResult<T> = Result<T, DiffFileError>;
 pub trait Diff: Sized {
     fn append(
         &mut self,
-        hashes: PostHashes,
-        db_ref: DbPostRef,
-    ) -> DiffResult<(PostHashes, DbPostRef)>;
+        hashes: &PostHashes,
+        db_ref: &DbPostRef,
+    ) -> DiffResult<()>;
 
-    fn apply_diff(&self, coll: &mut DbRefCollection) -> DiffResult<()>;
+    fn drain(&mut self) -> DiffResult<Vec<DbPostRefSerialized>>;
 }
 
-//todo: Open file for every write, or force user to apply the existing diff to the database before opening a new one?
-//todo: test error: thread 'legacy_database::index::tests::new::when_contains_deleted_post_should_add_to_deleted' has overflowed its stack
-
-pub struct DiffFile;
+pub struct DiffFile(File);
 
 impl DiffFile {
-    fn deserialize(file: &File) -> Vec<DbPostRefSerialized> {
-        let buf = BufReader::new(file);
-        buf.lines()
-            .map(|l| l.expect("Invalid diff file!"))
-            .map(|line| DbPostRefSerialized::deserialize(&line).expect("Invalid diff file!"))
-            .collect()
+    pub fn new() -> DiffResult<Self> {
+        let file = Self::create_file()?;
+        Ok(DiffFile(file))
+    }
+
+    fn create_file() -> io::Result<File> {
+        let file_path = Path::new(DIFF_FILENAME);
+        if !file_path.exists() {
+            File::create(&file_path)?;
+        }
+        let file = OpenOptions::new().append(true).create(true).read(true).open(&file_path)?;
+        Ok(file)
     }
 }
 
 impl Diff for DiffFile {
     fn append(
         &mut self,
-        hashes: PostHashes,
-        db_ref: DbPostRef,
-    ) -> DiffResult<(PostHashes, DbPostRef)> {
+        hashes: &PostHashes,
+        db_ref: &DbPostRef,
+    ) -> DiffResult<()> {
         let serialized_obj = DbPostRefSerialized::new(hashes, db_ref);
         let serialized_string = serialized_obj.serialize()?;
-        //self.0.write_all(format!("{}\n", serialized_string).as_bytes())?;
-
-        Ok(serialized_obj.split())
-    }
-
-    fn apply_diff(&self, coll: &mut DbRefCollection) -> DiffResult<()> {
-        let diff_file = File::open(DIFF_FILENAME);
-        let refs = match diff_file {
-            Ok(file) => Self::deserialize(&file),
-            Err(io) => match io.kind() {
-                std::io::ErrorKind::NotFound => iter::empty().collect(),
-                _ => return Err(io.into()),
-            },
-        };
-
-        //todo Apply parsed refs to collection
+        let mut file = Self::create_file()?;
+        file.write_all(format!("{}\n", serialized_string).as_bytes())?;
 
         Ok(())
     }
+
+    fn drain(&mut self) -> DiffResult<Vec<DbPostRefSerialized>>{
+        let diff_file = Self::create_file()?;
+        let buf = BufReader::new(&diff_file);
+
+        let result = buf.lines()
+            .map(|l| l.expect("Invalid diff file!"))
+            .map(|line| DbPostRefSerialized::deserialize(&line).expect("Invalid diff file!"))
+            .collect();
+        mem::drop(diff_file);
+        fs::remove_file(DIFF_FILENAME)?;
+
+        Ok(result)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
 }
