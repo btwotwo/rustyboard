2fd71518491cdf376cf79c52485d989b85a8ca1a
btwotwo <10519967+btwotwo@users.noreply.github.com>
2021-07-26 19:37:03 +0000

    Add new method to the `Database` trait

--- src/database/src/post_database.rs
+++ src/database/src/post_database.rs
@@ -4,6 +4,7 @@ use std::error::Error;
 pub trait Database {
     type Error: Error;
 
-    fn put_post(&mut self, post: Post, allow_reput: bool) -> Result<(), Self::Error>;
+    fn put_post(&mut self, post: Post) -> Result<(), Self::Error>;
     fn get_post(&self, hash: String) -> Option<Post>;
+    fn update_post(&mut self, post: Post) -> Result<(), Self::Error>;
 }
