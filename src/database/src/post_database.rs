9babe71a49b0b9cd0f1f771a2ac7d0e1104bc183
btwotwo <10519967+btwotwo@users.noreply.github.com>
2021-07-28 18:45:35 +0000

    Wrap `get_post` into `Result`

--- src/database/src/post_database.rs
+++ src/database/src/post_database.rs
@@ -5,6 +5,6 @@ pub trait Database {
     type Error: Error;
 
     fn put_post(&mut self, post: Post) -> Result<(), Self::Error>;
-    fn get_post(&self, hash: String) -> Option<Post>;
+    fn get_post(&self, hash: String) -> Result<Option<Post>, Self::Error>;
     fn update_post(&mut self, post: Post) -> Result<(), Self::Error>;
 }
