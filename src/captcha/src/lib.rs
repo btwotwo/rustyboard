ec1efde8b4027f15c4c461c1254d6930b51df53c
btwotwo <10519967+btwotwo@users.noreply.github.com>
2022-01-03 20:24:51 +0000

    Add captcha image generation, tests stub

--- src/captcha/src/lib.rs
+++ src/captcha/src/lib.rs
@@ -2,10 +2,13 @@ mod util;
 
 use ed25519::Error;
 use ed25519_dalek::{ExpandedSecretKey, PublicKey, Verifier, PUBLIC_KEY_LENGTH, SECRET_KEY_LENGTH};
+use image::{hdr::Rgbe8Pixel, GenericImage, GenericImageView, ImageBuffer, Rgb, RgbImage};
 use sha2::{Digest, Sha512};
 use util::byte_array_to_string;
+
 const SEED_LEN: usize = PUBLIC_KEY_LENGTH;
 const IMAGE_LEN: usize = 125;
+const CAPTCHA_LEN: usize = 189;
 
 pub struct Captcha {
     public_key: PublicKey,
@@ -14,21 +17,19 @@ pub struct Captcha {
 }
 
 pub struct Seed(Box<[u8; SEED_LEN]>);
-pub struct CaptchaImage(Box<[u8; IMAGE_LEN]>);
+pub type CaptchaImage = RgbImage;
 
 impl Captcha {
-    pub fn new(captcha: [u8; 189]) -> Result<Self, Error> {
+    pub fn new(captcha: [u8; CAPTCHA_LEN]) -> Result<Self, Error> {
         let public_key = PublicKey::from_bytes(&captcha[0..32])?;
         let seed = Seed(Box::new(
             captcha[32..64]
                 .try_into()
                 .expect("Seed should be 32 bytes long"),
         ));
-        let image = CaptchaImage(Box::new(
-            captcha[64..]
-                .try_into()
-                .expect("Image should be 189 bytes long"),
-        ));
+
+        let image_bytes: [u8; IMAGE_LEN] = captcha[64..189].try_into().expect("image is too long or short");
+        let image = build_image(&image_bytes);
 
         Ok(Captcha {
             public_key,
@@ -63,10 +64,54 @@ impl Captcha {
         let hash = hasher.finalize();
 
         let mut decrypted_seed = [0u8; SECRET_KEY_LENGTH];
-        for (i, byte) in self.seed.0.iter().enumerate() {
+            for (i, byte) in self.seed.0.iter().enumerate() {
             decrypted_seed[i] = *byte ^ hash[i & 63];
         }
 
         ExpandedSecretKey::from_bytes(&decrypted_seed).ok()
     }
+}
+
+fn build_image(image_bytes: &[u8; IMAGE_LEN]) -> RgbImage {
+    const WIDTH: u32 = 50;
+    const HEIGHT: u32 = 20;
+    const BLACK: Rgb<u8> = Rgb([0, 0, 0]);
+    const WHITE: Rgb<u8> = Rgb([255, 255, 255]);
+
+    let mut img = RgbImage::new(WIDTH, HEIGHT);
+    let mut bii = 0;
+    let mut byi = 0;
+
+    let (width, height) = img.dimensions();
+
+    for x in 0..width {
+        for y in 0..height {
+            let color = if (image_bytes[byi] & (1 << bii)) != 0 {
+                BLACK
+            } else {
+                WHITE
+            };
+
+            bii += 1;
+
+            if bii >= 8 {
+                bii = 0;
+                byi += 1;
+            }
+
+            img.put_pixel(x, y, color);
+        }
+    }
+
+    img
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::{Read, Seek, SeekFrom};
+    use std::fs::*;
+    const CAPTCHA_OFFSET: u8 = 1;
+    const CAPTCHA_ANSWER: &str = "r3go4";  
+    //todo signature tests
 }
